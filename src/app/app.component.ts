88f28a87ae7e8ed3b838f186b3e0d06ae76e563c
FenneDutchGrit <99078521@mydavinci.nl>
2025-01-08 14:30:11 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = "fenne's portfolio";
-  python: string = 'assets/images/python.png';
-  html: string = 'assets/images/html.png';
-  css: string = 'assets/images/css.png';
-  js: string = 'assets/images/javascript.png';
-  csharp: string = 'assets/images/csharp.png';
-
 }
 
